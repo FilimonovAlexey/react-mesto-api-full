eb8bc9df9834e0021d3bf551779808bf2bc9ebe0
Алексей Филимонов <alexeyf08@gmail.com>
2023-01-17 23:06:00 +0700

    добавлен пакет cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
+const { cors } = require('cors');
 const bodyParser = require('body-parser');
 const auth = require('./middlewares/auth');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.post(
   '/signup',
