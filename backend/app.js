6c7ec79192e12a84c49937ee69808febd1543ea7
Алексей Филимонов <alexeyf08@gmail.com>
2023-01-19 20:45:37 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,9 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
 app.post(
   '/signup',
